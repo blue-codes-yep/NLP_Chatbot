d1b0727873b98a792307312b478f38c5cacf5446
linzblue1 <linzblue5@gmail.com>
2022-07-25 00:10:57 -0500

    Changes before refactor

--- spacychatbot/spacychatbot/views.py
+++ spacychatbot/spacychatbot/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import redirect, render
 from django.shortcuts import HttpResponse
+from .forms import messageForm
+from .main import get_weather, take_user_input
 
 
 def home(request):
-    return render(request, 'home.html')
+    form = messageForm()
+
+    # Get the message to display to view.
+    if request.method == "POST":
+        rawText = request.POST.get('message', '')
+        context = {'rawText': rawText}
+        return render(request, 'home.html', {'form': form, 'context': context['rawText']})
+    else:
+        return render(request, 'home.html', {'form': form})
